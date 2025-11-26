e14a1baa4d6e20b70d9a60fc103220d218931f4d
oskwon-ship-it <os.kwon@mycommerc.com>
2025-11-26 11:18:13 +0900

    라우터 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+// BrowserRouter(Router)는 main.jsx에 이미 있으므로 여기서 뺍니다.
+
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import OrderManagement from './pages/OrderManagement'; // 1. 이 줄 추가!
+import OrderManagement from './pages/OrderManagement';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/orders" element={<OrderManagement />} /> {/* 2. 이 줄 추가! */}
-      </Routes>
-    </Router>
+    // <Router> 태그 제거됨
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/orders" element={<OrderManagement />} />
+    </Routes>
+    // </Router> 태그 제거됨
   );
 }
 
