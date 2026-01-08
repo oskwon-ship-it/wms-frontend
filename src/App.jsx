1887f2f759a4041a1f762e739201e27b78bcf388
oskwon-ship-it <os.kwon@mycommerc.com>
2026-01-08 16:39:16 +0900

    44

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // ★ Router 제거, Routes/Route만 남김
 
-// 기존 페이지들 import
+// 페이지들 import
 import Dashboard from './pages/Dashboard';
 import OrderEntry from './pages/OrderEntry';
-import InventoryManagement from './pages/InventoryManagement'; // Inventory -> InventoryManagement로 추정
-import InboundManagement from './pages/InboundManagement'; // 파일 목록에 보여서 추가함
-import InventoryHistory from './pages/InventoryHistory';   // 파일 목록에 보여서 추가함
-import OrderProcessing from './pages/OrderProcessing';     // 파일 목록에 보여서 추가함
+import InventoryManagement from './pages/InventoryManagement'; 
+import InboundManagement from './pages/InboundManagement'; 
+import InventoryHistory from './pages/InventoryHistory';   
+import OrderProcessing from './pages/OrderProcessing';     
 import Login from './pages/Login';
 
-// ★ 새로 만든 진단실 페이지 import
+// 새로 만든 진단실 페이지
 import ApiTester from './pages/ApiTester'; 
 
 function App() {
   return (
-    <Router>
-      <Routes>
+    // ★ <Router> 태그 삭제함 (이미 main.jsx에 있어서 충돌남)
+    <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         
@@ -29,10 +29,9 @@ function App() {
         <Route path="/inventory-history" element={<InventoryHistory />} />
         <Route path="/inbound" element={<InboundManagement />} />
         
-        {/* ★ 새로 추가된 API 진단실 경로 (여기에 추가!) */}
+        {/* API 진단실 */}
         <Route path="/api-test" element={<ApiTester />} />
-      </Routes>
-    </Router>
+    </Routes>
   );
 }
 
